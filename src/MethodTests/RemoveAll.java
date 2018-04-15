46495c412ae6497e0fe2ce664e28072c2c86c65c
Hoymm <Damian.Andrzej.Muca@gmail.com>
2018-04-15 10:06:34 +0200

    Add method to List.removeAll() test

--- src/MethodTests/RemoveAll.java
+++ src/MethodTests/RemoveAll.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class RemoveAll extends ListTesting{
     private final static String METHOD_NAME = ".removeAll()";
 
-    public static void listRemoveAll_shouldResultInEmptyList(List actualList) {
+    public static void list12345_removeAll124_shouldResultInList35(List actualList) {
         // given
         actualList.addAll(Arrays.asList(1,2,3,4,5));
         List expectedList35 = Arrays.asList(3,5);
@@ -15,5 +15,15 @@ public class RemoveAll extends ListTesting{
         actualList.removeAll(Arrays.asList(1,2,4));
         // then
         assertListsEquals(actualList.getClass() + METHOD_NAME, expectedList35, actualList);
+    }
+
+    public static void list11112_removeAll1_shouldResultInList2(List actualList) {
+        // given
+        actualList.addAll(Arrays.asList(1,1,1,1,2));
+        List expectedList2 = Arrays.asList(2);
+        // when
+        actualList.removeAll(Arrays.asList(1));
+        // then
+        assertListsEquals(actualList.getClass() + METHOD_NAME, expectedList2, actualList);
     }
 }
