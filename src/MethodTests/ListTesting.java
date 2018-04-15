c47cd5af7d2ec9ce70995faf50a2369b5b3eea5b
Hoymm <Damian.Andrzej.Muca@gmail.com>
2018-04-15 09:06:02 +0200

    Refactor ListTesting.class

--- src/MethodTests/ListTesting.java
+++ src/MethodTests/ListTesting.java
@@ -1,19 +1,24 @@
 package MethodTests;
 
+import java.util.Iterator;
 import java.util.List;
 
 abstract class ListTesting {
     static <T> void assertEquals(String comment, T expected, T actual) {
-        assert expected.equals(actual) :
-                "\u001B[31m" + comment + " Expected value: " + "\u001B[32m" + expected + "\u001B[31m"
-                        + ", retrieved: " + "\u001B[30m" + actual + "\u001B[31m";
+        assert expected.equals(actual) : printMessageNotEquals(comment, expected, actual);
     }
 
-    static <T> void assertListsEquals(String comment, List expected, List actual) {
-        for (int i = 0; i < expected.size(); ++i){
-            assert actual.get(i) == expected.get(i)
-                    : (comment +  "\nExpected : " + "\u001B[32m" + expected + "\u001B[31m" +", Actual: "
-                    + "\u001B[30m" + actual + "\u001B[31m");
-        }
+    static void assertListsEquals(String comment, List expectedList, List actualList) {
+        Iterator itExpected = expectedList.iterator();
+        Iterator itActual = actualList.iterator();
+
+        assert expectedList.size() == actualList.size() : printMessageNotEquals(comment, expectedList, actualList);
+        while (itExpected.hasNext() && itExpected.hasNext())
+            assert itExpected.next() == itActual.next() : printMessageNotEquals(comment, expectedList, actualList);
+    }
+
+    private static <T> String printMessageNotEquals(String comment, T expected, T actual) {
+        return comment +  "\nExpected list: " + "\u001B[32m" + expected + "\u001B[31m" +", Actual list: "
+        + "\u001B[30m" + actual + "\u001B[31m";
     }
 }
