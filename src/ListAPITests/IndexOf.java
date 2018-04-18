0b0615d2575472b587b2e77bf89888b5009bc997
Damian <Damian.Andrzej.Muca@gmail.com>
2018-04-18 14:10:20 +0200

    Add test to .indexOf() method

--- src/ListAPITests/IndexOf.java
+++ src/ListAPITests/IndexOf.java
@@ -13,10 +13,18 @@ public class IndexOf extends ListTesting{
         list.add(obj1);
         list.add(obj2);
         list.add(obj3);
-        int expectedIndexOfObj2IsValue1 = 1;
+        int indexOfObj2ShouldReturn1 = 1;
         // when
         int actualIndexOfObj2 = list.indexOf(obj2);
         // then
-        assertEquals(list.getClass() + METHOD_NAME, expectedIndexOfObj2IsValue1, actualIndexOfObj2);
+        assertEquals(list.getClass() + METHOD_NAME, indexOfObj2ShouldReturn1, actualIndexOfObj2);
+    }
+
+    public static void tryingGetElementThatDoesNotExits_shouldReturnNegative1(List list){
+        // given
+        // when
+        int indexOfNotExistingElement = list.indexOf("NO SUCH OBJECT");
+        // then
+        assertEquals(list.getClass() + METHOD_NAME, -1, indexOfNotExistingElement);
     }
 }
